f996c8a15d69aa58369be29322fe2ee86c671606
MetalFaceJr <143476388+MetalFaceJr@users.noreply.github.com>
2026-03-24 12:21:23 -0400

    Add metadata for layout with title and description

---
+++ app/layout.js
@@ -0,0 +1,4 @@
+export const metadata = {
+  title: 'Calhoun GA Grappling Club | Jiu-Jitsu & Submission Wrestling',
+  description: 'Premier Submission Grappling and Brazilian Jiu-Jitsu in Calhoun, Georgia. Join us to build strength, learn technique, and earn respect.',
+}
